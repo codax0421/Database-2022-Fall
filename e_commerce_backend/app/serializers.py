2e82e416cce46a3ce53e20d63490545e83db1d80
codax0421 <59133030+codax0421@users.noreply.github.com>
2022-12-15 15:27:59 +0800

    刪掉backenv 檔案 (#13)
    * home page/product detail and transaction
    
    * changes
    
    * changes
    
    * Update .gitignore
    
    * Delete e_commerce_backend/backenv directory
    
    * Update .gitignore
    
    * prettier
    
    * changes

---
+++ e_commerce_backend/app/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from .models import Product, Tag, Transaction
+
+from.models import Product_Comment
+from .models import Category
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Tag
+        fields = '__all__' 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Category
+        fields = '__all__' 
+        
+class ProductSerializer(serializers.ModelSerializer):
+    tag = TagSerializer(read_only=True, many=True)
+
+    class Meta :
+            model = Product
+            fields =  ['id' , 'name' , 'seller' , 'category' ,  'price' ,'description' ,'image' ,'tag']
+
+
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Transaction
+        fields = ['id','buyer','seller','product','date']
+
+class ProductCommentSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Product_Comment
+        fields = ['id','buyer' , 'comment' ]
+
+
--- e_commerce_frontend/src/components/pages/Home.js
+++ e_commerce_frontend/src/components/pages/Home.js
@@ -1,6 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Select, Space, Avatar, Card } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from "../../axios";
 
+const { Meta } = Card;
 const Home = () => {
-  return <div>Home Page</div>;
+  const [productHome, setproductHome] = useState([]);
+  const [tag, setTag] = useState([]);
+  const [category, setCategory] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios.get("products/").then((res) => {
+      console.log(res.data.data);
+      setproductHome(res.data.data);
+    });
+    axios.get("tag/").then((res) => {
+      setTag(res.data.data);
+    });
+
+    axios.get("category/").then((res) => {
+      setCategory(res.data.data);
+    });
+  }, []);
+
+  const navigateToProduct = (e, id, name) => {
+    // 👇️ navigate to /contacts
+    console.log(name);
+    navigate("/product/" + id, {
+      state: {
+        productId: id,
+        productName: name,
+      },
+    });
+  };
+
+  return (
+    <>
+      <>
+        <Select
+          mode="multiple"
+          style={{ width: "10%", marginTop: "20px", marginLeft: "30px" }}
+          options={category.map((item) => ({
+            value: item.name,
+            label: item.name,
+          }))}
+        />
+        <Select
+          mode="multiple"
+          style={{ width: "10%", marginTop: "20px", marginLeft: "30px" }}
+          options={tag.map((item) => ({
+            value: item.name,
+            label: item.name,
+          }))}
+        />
+      </>
+      <div style={{ display: "flex", flexWrap: "wrap", margin: "70px" }}>
+        {productHome.map((product) => {
+          return (
+            <Card
+              style={{ width: "300px", margin: "10px" }}
+              value={product.id}
+              key={product.id}
+              cover={
+                <img
+                  alt={product.alt}
+                  src={"http://127.0.0.1:8000/" + product.image}
+                  onClick={(e) =>
+                    navigateToProduct(e, product.id, product.name)
+                  }
+                />
+              }
+              actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+              ]}
+            >
+              <Meta
+                title={product.name}
+                description={"$" + product.description}
+              />
+            </Card>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 export default Home;
