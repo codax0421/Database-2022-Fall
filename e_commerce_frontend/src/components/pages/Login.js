00558da7d8fdafec78e9330a871440328a52b29d
Bosh-Kuo <79684268+Bosh-Kuo@users.noreply.github.com>
2022-12-23 14:20:27 +0800

    Login & Register 純畫面 (#17)
    * Login & Register UI
    
    * set Snackbar's onClose callback function

--- e_commerce_frontend/src/components/pages/Login.js
+++ e_commerce_frontend/src/components/pages/Login.js
@@ -1,6 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("Successfully log in!");
+  const handleAlert = (data) => {
+    if (
+      data.get("username").length === 0 ||
+      data.get("password").length === 0
+    ) {
+      setAlertType("error");
+      setAlertMessage("There are unfilled forms!");
+    } else {
+      setAlertType("success");
+      setAlertMessage("Successfully log in!");
+    }
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    handleAlert(data);
+    console.log({
+      username: data.get("username"),
+      password: data.get("password"),
+    });
+    setAlertOpen(true);
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          LogIn
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username" // 顯示於頁面輸入框上
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            LogIn
+          </Button>
+          <Snackbar
+            open={alertOpen}
+            autoHideDuration={1500}
+            onClose={() => {
+              setAlertOpen(false);
+            }}
+          >
+            <Alert severity={alertType} sx={{ width: "100%" }}>
+              {alertMessage}
+            </Alert>
+          </Snackbar>
+          <Grid container>
+            <Grid item xs></Grid>
+            <Grid item>
+              <Link href="register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 export default Login;
--- e_commerce_frontend/src/components/pages/Register.js
+++ e_commerce_frontend/src/components/pages/Register.js
@@ -1,6 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Register = () => {
-  return <div>Register Page</div>;
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("Successfully sign up!");
+  const handleAlert = (data) => {
+    if (
+      data.get("username").length === 0 ||
+      data.get("email").length === 0 ||
+      data.get("password").length === 0 ||
+      data.get("password-check").length === 0
+    ) {
+      setAlertType("error");
+      setAlertMessage("There are unfilled forms!");
+    } else if (data.get("password-check") !== data.get("password")) {
+      setAlertType("error");
+      setAlertMessage("Check your password again");
+    } else {
+      setAlertType("success");
+      setAlertMessage("Successfully sign up!");
+    }
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    handleAlert(data);
+    console.log({
+      username: data.get("username"),
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    setAlertOpen(true);
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password-check"
+                label="Password (check again)"
+                type="password"
+                id="password-check"
+                autoComplete="password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Snackbar
+            open={alertOpen}
+            autoHideDuration={1500}
+            onClose={() => {
+              setAlertOpen(false);
+            }}
+          >
+            <Alert severity={alertType} sx={{ width: "100%" }}>
+              {alertMessage}
+            </Alert>
+          </Snackbar>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 export default Register;
